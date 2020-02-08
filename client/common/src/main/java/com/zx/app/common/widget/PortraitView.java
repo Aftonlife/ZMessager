51e9b8d8efc15707a489698513d4165b2c86008f
afton <jsafton@itcom888.com>
2020-02-08 19:52:23 +0800

    基础Style添加

---
+++ client/common/src/main/java/com/zx/app/common/widget/PortraitView.java
@@ -0,0 +1,24 @@
+package com.zx.app.common.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * author Afton
+ * date 2020/2/8
+ */
+public class PortraitView extends CircleImageView {
+    public PortraitView(Context context) {
+        super(context);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
